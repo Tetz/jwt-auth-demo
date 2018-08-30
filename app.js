94e8bc0e56f8627c42151f617e9a3227545ac675
Tetsuro Takemoto <t@ineeza.com>
2018-08-30 18:54:15 +0900

    Initial commit :rocket:

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const app = express();
+const UserModel = require('./model/model');
+
+mongoose.connect('mongodb://127.0.0.1:27017/passport-jwt', { useNewUrlParser: true });
+mongoose.connection.on('error', error => console.log(error) );
+mongoose.Promise = global.Promise;
+
+require('./auth/auth');
+
+app.use( bodyParser.urlencoded({ extended : false }) );
+
+const routes = require('./routes/routes');
+const secureRoute = require('./routes/secure-routes');
+
+app.use('/', routes);
+//We plugin our jwt strategy as a middleware so only verified users can access this route
+app.use('/user', passport.authenticate('jwt', { session : false }), secureRoute );
+
+//Handle errors
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error : err });
+});
+
+module.exports = app;
